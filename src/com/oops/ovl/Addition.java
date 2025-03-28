034ad708f6750c32fd97566a99cff14f5e57fe1b
Arpita Roy <100989922+Arpita23r@users.noreply.github.com>
2025-03-28 20:40:26 +0530

    Add files via upload

---
+++ src/com/oops/ovl/Addition.java
@@ -0,0 +1,13 @@
+package com.oops.ovl;
+
+public class Addition {
+public void add(int a,int b ){
+	System.out.println(a+b);
+}
+public  void add(double a,double b) {
+	System.out.println(a+b);
+}
+public void add(String a,String b) {
+	System.out.println(a+b);
+}
+}
---
+++ src/com/oops/ovl/Main.java
@@ -0,0 +1,14 @@
+package com.oops.ovl;
+
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int c=sc.nextInt();
+	int d=sc.nextInt();
+	Addition result=new Addition();
+	result.add(c, d);
+	
+}
+}
